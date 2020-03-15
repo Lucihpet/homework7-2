d03a22e202689477fe03f8855f0ad043e1571690
Lucihpet <59805094+Lucihpet@users.noreply.github.com>
2020-03-14 18:35:48 -0700

    Still need to test other functions.

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -11,30 +11,105 @@ public class MaxHeap implements Heap {
     // build a heap based on data
     // to be implemented in O(nlogn)
     public void MaxHeapLogN(Integer[] data) {
-        // homework
+        for (int i = 0; i < data.length; i++) {
+            boolean add = add(data[i]);
+        }
     }
 
     // build a heap based on data
     // to be implemented in O(n)
     public void MaxHeapN(Integer[] data) {
-        // homework
+        if (data.length == 0)
+            return;
+
+        size = data.length;
+
+        for (int i = (size/2)-1; i >= 0; i--) {
+            maxHeapify(data,i);
+        }
     }
 
     // add an item to the heap
     public boolean add(Integer item) {
-        // homework
+        if (this.size == this.data.length)
+            return false;
+
+        this.data[size] = item;
+        int n = size;
+        this.size++;
+
+        while (n != 0 && this.data[n] > this.data[(n-1)/2]) {
+            int temp = this.data[(n-1)/2];
+            this.data[(n-1)/2] = this.data[n];
+            this.data[n] = temp;
+            n = (n-1)/2;
+        }
+
         return false;
+    }
+
+    public void maxHeapify(Integer[] data, int i) {
+        //
+        int left = 2*i + data.length%2;
+        int right = 2*i + 1 + data.length%2;
+        int largest = i;
+
+        if (left < data.length && data[left] > data[right])
+            largest = left;
+
+        if (right < data.length && data[right] > data[left])
+            largest = right;
+
+        if (largest != i) {
+            int temp = data[i];
+            data[i] = data[largest];
+            data[largest] = temp;
+
+            maxHeapify(data, largest);
+        }
     }
 
     // return the max item in the heap
     public Integer get() {
-        // homework
-        return null;
+        if (this.size == 0)
+            return null;
+
+        return this.data[0];
     }
 
     // remove the root item
     public Integer pop() {
-        // homework
-        return null;
+        if (this.size == 0)
+            return null;
+
+        int returnInt = this.data[0];
+
+        for (int i = 0; i < this.size - 1; i++) {
+            data[i] = data[i+1];
+        }
+
+        size--;
+
+        return returnInt;
+    }
+
+    public boolean equals (Integer[] data) {
+        for (int i = 0; i < this.data.length; i++) {
+            if (this.data[i] != data[i])
+                return false;
+        }
+
+        return true;
+    }
+
+    public String toString() {
+        String returnString = "[";
+        for (int i = 0; i < size; i++) {
+            returnString = returnString + this.data[i];
+            if (i != size -1)
+                returnString += " ";
+        }
+        returnString += "]";
+        return returnString;
     }
 }
--- src/MaxHeapTest.java
+++ src/MaxHeapTest.java
@@ -1,5 +1,55 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MaxHeapTest {
-    // homework
+
+    @Test
+    public void nLognTest() {
+        Integer[][] testData = {
+                {},
+                {2},
+                {3,6,3,2,6},
+                {3,4,6,5,8,2,1,9}
+        };
+
+        Integer[][] expected = {
+                {},
+                {2},
+                {6,6,3,2,3},
+                {9,8,4,6,5,2,1,3}
+        };
+
+        for (int i = 0; i < testData.length; i++) {
+            MaxHeap actual = new MaxHeap(testData[i].length);
+
+            actual.MaxHeapLogN(testData[i]);
+
+            assertTrue(actual.equals(expected[i]));
+        }
+    }
+
+    @Test
+    public void nTest() {
+        Integer[][] testData = {
+                {},
+                {2},
+                {3,6,3,2,6},
+                {3,4,6,5,8,2,1,9}
+        };
+
+        Integer[][] expected = {
+                {},
+                {2},
+                {6,6,3,2,3},
+                {9,8,6,5,3,2,1,4}
+        };
+
+        for (int i = 0; i < testData.length; i++) {
+            MaxHeap maxHeap = new MaxHeap(testData.length);
+            maxHeap.MaxHeapN(testData[i]);
+
+            assertEquals(expected[i], testData[i]);
+        }
+    }
 }
